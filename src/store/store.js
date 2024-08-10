e0042da40537a1841c1a95d034f4922529ff7722
Uzairkazi695 <167708998+Uzairkazi695@users.noreply.github.com>
2024-08-10 16:00:26 +0530

    update

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
+import cartSlice from "./cartSlice";
 
 const store = configureStore({
-    reducer:{
-        auth: authSlice,
-    }
-})
+  reducer: {
+    auth: authSlice,
+    cart: cartSlice,
+  },
+});
 
-export default store
+export default store;
